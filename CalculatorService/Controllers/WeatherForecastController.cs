612479980a901599f26fa0193c67136670ca6cd4
prabhacognizant <Prabhakaran.A@cognizant.com>
2026-01-30 12:31:35 +0530

    Update WeatherForecastController.cs

--- CalculatorService/Controllers/WeatherForecastController.cs
+++ CalculatorService/Controllers/WeatherForecastController.cs
@@ -15,6 +15,7 @@ public class WeatherForecastController : ControllerBase
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
+    
         _logger = logger;
     }
 
